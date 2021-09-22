0452f1a180d0db838d6cfe3acc934a97ddf73174
vineeths2021 <vineethsoman.2011@gmail.com>
2021-09-22 16:57:34 +0530

    Vineeth | Added testcase for delete

--- Demo-for-Product-CRUD/src/test/java/com/example/product/service/ProductServiceMockitoTest.java
+++ Demo-for-Product-CRUD/src/test/java/com/example/product/service/ProductServiceMockitoTest.java
@@ -13,10 +13,10 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.context.SpringBootTest.WebEnvironment;
 import org.junit.jupiter.api.BeforeEach;
 import static org.mockito.ArgumentMatchers.any;
+import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.mockito.Mockito.when;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -27,97 +27,91 @@ import com.example.product.repository.ProductRepository;
 
 /**
  * Service Layer unit testcases with Mockito
+ * 
  * @author Vineeth
  *
  */
 @ExtendWith(MockitoExtension.class)
 public class ProductServiceMockitoTest {
-	
-	
+
 	@InjectMocks
-	private ProductService service=new ProductService();
-	
+	private ProductService service = new ProductService();
+
 	@Mock
 	private ProductRepository repository;
-	
 
-	
 	@DisplayName(" Product save Success")
 	@Test
-	public void testSaveProductServiceSuccess(){
-		//create a product
-		Product product=new Product();
+	public void testSaveProductServiceSuccess() {
+		// create a product
+		Product product = new Product();
 		product.setProductname("Car");
 		product.setProductrate(120000.0);
-		
-		
+
 		when(repository.save(any(Product.class))).thenReturn(product);
-		
-		//save the product
-		Product newProduct=service.saveProduct(product);
-		
-		//verify the save
-		assertEquals("Car",newProduct.getProductname());
+
+		// save the product
+		Product newProduct = service.saveProduct(product);
+
+		// verify the save
+		assertEquals("Car", newProduct.getProductname());
 	}
-	
+
 	@DisplayName(" Product get Success")
 	@Test
-	public void testGetProductServiceSuccess(){
-		
-		String id="1";
-		//create a product
-		Product product=new Product();
+	public void testGetProductServiceSuccess() {
+
+		String id = "1";
+		// create a product
+		Product product = new Product();
 		product.setProductname("Car");
 		product.setProductrate(120000.0);
 		Optional<Product> returnProduct = Optional.of((Product) product);
-		
+
 		when(repository.findById(Long.valueOf(id))).thenReturn(returnProduct);
-		
-		//save the product
-		Product newproduct=service.getProduct(id);
-		
-		//verify the save
-		assertEquals("Car",newproduct.getProductname());
+
+		// get the product
+		Product newproduct = service.getProduct(id);
+
+		// verify the save
+		assertEquals("Car", newproduct.getProductname());
 	}
-	
+
 	@DisplayName(" Product get All Success")
 	@Test
-	public void testGetAllProductServiceSuccess(){
-		
-		
-		//create a product
-		Product product=new Product();
+	public void testGetAllProductServiceSuccess() {
+
+		// create a product
+		Product product = new Product();
 		product.setProductname("Car");
 		product.setProductrate(120000.0);
 		List<Product> returnProduct = new ArrayList();
 		returnProduct.add(product);
 		when(repository.findAll()).thenReturn(returnProduct);
-		
-		//save the product
-		List<Product> newproductlist=service.getProducts();
-		
-		//verify the save
-	
-		 assertEquals(1, newproductlist.size());
+
+		// get all the product
+		List<Product> newproductlist = service.getProducts();
+
+		// verify the save
+
+		assertEquals(1, newproductlist.size());
 	}
+
 	@DisplayName(" Product delete Success")
 	@Test
-	public void testdeleteProductServiceSuccess(){
-		
-		
-		//create a product
-		Product product=new Product();
+	public void testdeleteProductServiceSuccess() {
+
+		final Product product = new Product();
 		product.setProductname("Car");
 		product.setProductrate(120000.0);
-		List<Product> returnProduct = new ArrayList();
-		returnProduct.add(product);
-		when(repository.findAll()).thenReturn(returnProduct);
-		
-		//save the product
-		List<Product> newproductlist=service.getProducts();
-		
-		//verify the save
-	
-		 assertEquals(1, newproductlist.size());
+		Optional<Product> optionalEntityType = Optional.of(product);
+		when(repository.findById(1L)).thenReturn(optionalEntityType);
+
+		// when
+		service.deleteProduct("1");
+
+		// then
+		verify(repository, times(1)).delete(product);
+
 	}
 }
